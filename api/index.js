f1e222b0a849e84acfbc70899adb79b5f0a3542e
Urjashee Shaw <urjashee09@gmail.com>
2023-11-25 22:35:48 +0530

    connected to mongo

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import e from "express";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to MongoDB!')
+}).catch((err) => {
+    console.error(err)
+})
 
 const app = express();
 

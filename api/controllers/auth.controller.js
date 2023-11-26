7e74316062b2a67299fa82693de51608c3cb3557
Urjashee Shaw <urjashee09@gmail.com>
2023-11-26 22:06:52 +0530

    sign in api route with jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
+import {errorHandler} from '../utils/error.js';
+import {generateToken} from "../utils/generateToken.js";
+
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -17,5 +19,24 @@ export const signup = async (req, res, next) => {
     } catch (err) {
         next(err);
     }
-
+}
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email})
+        if (!validUser)
+            return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword)
+            return next(errorHandler(401, 'Wrong credentials!'));
+        const token = generateToken(validUser)
+        const { password: pass, ...userInfo } = validUser._doc;
+        console.log("Token - " + token)
+        res
+            .cookie('access_token', token, {httpOnly: true})
+            .status(200)
+            .json(userInfo);
+    } catch (e) {
+        next(e);
+    }
 }

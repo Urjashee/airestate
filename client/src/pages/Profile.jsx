abb4399b0db0f03f6af119154f7341764ccbd1c1
Urjashee Shaw <urjashee09@gmail.com>
2023-11-28 11:14:00 +0530

    image upload and property ui complete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -178,6 +178,12 @@ function Profile(props) {
                 >
                     {loading ? 'Loading...' : 'Update'}
                 </button>
+                <Link
+                    className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+                    to={'/create-listing'}
+                >
+                    Create Listing
+                </Link>
             </form>
             <div className='flex justify-between mt-5'>
                 <span
